b308b5d34a100868c40ae2b97fca4840dd814b95
Le Vu <levu08072002@gmail.com>
2022-07-21 11:21:33 +0700

    update enviroment

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
-
+import 'dotenv/config'
+import chalk from 'chalk'
 const app = express()
 
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
 
-const PORT = 5000
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT ${PORT}`);
+const successLog = chalk.bold.green
+const errorLog = chalk.bold.red
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(successLog(`Server is running`));
 })
 
+mongoose.connect(process.env.MONGODB_URL).then(() => console.log(successLog("Connect DB Successfully")))
+.catch(error => console.log(errorLog(error)))
 
